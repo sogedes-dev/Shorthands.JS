b690d7c775a5a2501db0564f0bca9e724862f5c7
hansschmucker <hansschmucker@gmail.com>
2026-03-02 17:58:18 +0100

    Update Array.prototype.distinct.js

--- Array.prototype.distinct.js
+++ Array.prototype.distinct.js
@@ -2,10 +2,10 @@
  * Returns all distinct values of an array
  */
 
-Array.prototype.distinct=function() {
+Object.defineProperty(Array.prototype,"distinct",{value:function distinct() {
     return this.reduce((p,a)=>{
         if(p.indexOf(a)<0)
             p.push(a);
         return p;
-    },[])
-};
+    },[]);
+}});
