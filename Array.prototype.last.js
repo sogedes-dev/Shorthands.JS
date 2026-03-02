8ab0ea6ac1231c6fb74f31cc1c5b01d241e60080
hansschmucker <hansschmucker@gmail.com>
2026-03-02 18:00:53 +0100

    Update Array.prototype.last.js

--- Array.prototype.last.js
+++ Array.prototype.last.js
@@ -1,10 +1,10 @@
 /*
     Returns the last element of an array with optional filtering and default value
  */
-Array.prototype.last=function(filter,def){
+Object.defineProperty(Array.prototype,"last",{value:function last(filter,def) {
     if(filter)
         var r= this.filter(filter);
     else
         var r=this;
     return r.length?r[r.length-1]:def;
-};
+}});
