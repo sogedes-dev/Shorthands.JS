9895432756cb47dc4ef60bbbaa8e1c5ad8d599e4
hansschmucker <hansschmucker@gmail.com>
2026-03-02 17:31:11 +0100

    Update Array.prototype.groupBy.js

--- Array.prototype.groupBy.js
+++ Array.prototype.groupBy.js
@@ -1,3 +1,9 @@
-Array.prototype.groupBy=function(grouper){
-  return Object.groupBy(this,grouper)
+Array.prototype.groupBy=function(args){
+  args=[...arguments];
+  
+  var data=Object.groupBy(this,typeof(args[0])=="function"?args[0]:function(field,line){ return line[field]; }.bind(null,args[0]) );
+  if(args.length>1)
+    for(var i in data)
+        data[i]=data[i].groupBy.apply(data[i],args.slice(1));
+  return data;
 };
