758058cd40c10d0c370e47caba8480f21648f84b
hansschmucker <hansschmucker@gmail.com>
2026-03-02 17:59:32 +0100

    Update Array.prototype.groupBy.js

--- Array.prototype.groupBy.js
+++ Array.prototype.groupBy.js
@@ -7,3 +7,13 @@ Array.prototype.groupBy=function(args){
         data[i]=data[i].groupBy.apply(data[i],args.slice(1));
   return data;
 };
+
+Object.defineProperty(Array.prototype,"groupBy",{value:function groupBy(args) {
+  args=[...arguments];
+  
+  var data=Object.groupBy(this,typeof(args[0])=="function"?args[0]:function(field,line){ return line[field]; }.bind(null,args[0]) );
+  if(args.length>1)
+    for(var i in data)
+        data[i]=data[i].groupBy.apply(data[i],args.slice(1));
+  return data;
+}});
