73013a034c3471b656a97ebf3cd0d66ee48be9d6
hansschmucker <hansschmucker@gmail.com>
2026-03-03 16:45:57 +0100

    Update Array.prototype.distinct.js

--- Array.prototype.distinct.js
+++ Array.prototype.distinct.js
@@ -2,10 +2,14 @@
  * Returns all distinct values of an array
  */
 
-Object.defineProperty(Array.prototype,"distinct",{value:function distinct() {
-    return this.reduce((p,a)=>{
-        if(p.indexOf(a)<0)
-            p.push(a);
-        return p;
-    },[]);
+Object.defineProperty(Array.prototype,"distinct",{value:function distinct(serialize) {
+    if(serialize){
+        return this.map(a=>a.toJson()).distinct().map(a=>a.asJson());
+    }else{
+        return this.reduce((p,a)=>{
+            if(p.indexOf(a)<0)
+                p.push(a);
+            return p;
+        },[]);
+    }
 }});
