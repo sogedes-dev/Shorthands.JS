8812676790bee5ef6e2b298e112106c1d8b61331
hansschmucker <hansschmucker@gmail.com>
2026-03-04 17:27:54 +0100

    Delete Array.prototype.asEntries.ts

--- Array.prototype.asEntries.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Returns all own properties of an object as an Array of name<->value pairs
- * @alias Array.prototype.asEntries
- * @returns [{string} name,{object} value][]
- */
-
-Array.prototype.asEntries=function asEntries(){return [["A",{}],["B",{}]]};
