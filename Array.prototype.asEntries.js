d2315e44dfca391874dd9c90d839b17b16afede0
schmucker-sogedes <61741735+schmucker-sogedes@users.noreply.github.com>
2026-03-04 16:27:09 +0100

    Update Array.prototype.asEntries.js

--- Array.prototype.asEntries.js
+++ Array.prototype.asEntries.js
@@ -1,3 +1,8 @@
+/**
+ * Returns all own properties of an object as an Array of name<->value pairs
+ * @alias Array.prototype.asEntries
+ * @returns [{string} name,{object} value][]
+ */
 Object.defineProperty(Array.prototype,"asEntries",{value:function asEntries() {
     return Object.fromEntries(this);
 }});
