dfd811b4ff0a7f443c6431dd180c01ef028f8ce4
hansschmucker <hansschmucker@gmail.com>
2026-03-05 10:20:57 +0100

    Update String.prototype.asXml.js

--- String.prototype.asXml.js
+++ String.prototype.asXml.js
@@ -1,7 +1,12 @@
 /**
- * Parses xml and returns the document xml
+ * Parses the current string as xml and returns the document xml
+ * @method
+ * @instance
+ * @memberOf String
+ * @name asXml
  * @returns {Element}
  */
+
 String.prototype.asXml=function(){
     return (new DOMParser().parseFromString(this, "application/xml")).documentElement;
 };
