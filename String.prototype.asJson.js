cb39d79ca42681af4aaf07ca1fdc45eab827e646
hansschmucker <hansschmucker@gmail.com>
2026-03-05 10:20:07 +0100

    Update String.prototype.asJson.js

--- String.prototype.asJson.js
+++ String.prototype.asJson.js
@@ -1,3 +1,12 @@
+/**
+ * Parses the current string as JSON
+ * @method
+ * @instance
+ * @memberOf String
+ * @name asJson
+ * @returns {object}
+ */
+
 String.prototype.asJson=function(){
     return JSON.parse(this);
 };
