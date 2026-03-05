1b305e51e1bd38ff8044306d08ac87cbb58d84e5
hansschmucker <hansschmucker@gmail.com>
2026-03-05 10:14:13 +0100

    Update Object.prototype.getProperties.js

--- Object.prototype.getProperties.js
+++ Object.prototype.getProperties.js
@@ -1,3 +1,15 @@
+/**
+ * Create a new object containing only the fields given
+ * @method
+ * @instance
+ * @memberOf Object
+ * @name getProperties
+ * @param propName1 {string} Property name
+ * @param propName2 {string} Property name
+ * @param propNameN {string} Property name
+ * @returns {Object}
+ */
+
 Object.defineProperty(Object.prototype,"getProperties",{value:function getProperties(args){
   var args=[...arguments];
   return Object.fromEntries(Object.entries(this).filter(a=>args.contains(a[0])));
