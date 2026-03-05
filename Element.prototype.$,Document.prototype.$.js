99e1271e67ecfe54a9c717696b418356d2a3431a
hansschmucker <hansschmucker@gmail.com>
2026-03-05 10:26:20 +0100

    Update Element.prototype.$,Document.prototype.$.js

--- Element.prototype.$,Document.prototype.$.js
+++ Element.prototype.$,Document.prototype.$.js
@@ -3,14 +3,25 @@
  * Otherwise returns all elements matching the selector in the subtree as array.
  * @method
  * @instance
- * @memberOf Element
  * @memberOf Document
  * @name $
  * @param selector {string} Selector to search for
  * @returns {Array|Element|null}
 */
-Element.prototype.$=Document.prototype.$=function(sel){
+Document.prototype.$=function(sel){
   return /#[^\s>~|]+$/.exec(sel)?
     this.querySelector(sel):
     [...this.querySelectorAll(sel)]
 };
+
+/**
+ * Returns an element matching the selector if the last part of the selector starts with #
+ * Otherwise returns all elements matching the selector in the subtree as array.
+ * @method
+ * @instance
+ * @memberOf Element
+ * @name $
+ * @param selector {string} Selector to search for
+ * @returns {Array|Element|null}
+*/
+Element.prototype.$=Document.prototype.$;
