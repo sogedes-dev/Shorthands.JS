97b3ee6270ba548b7cb901279272178ed098757c
hansschmucker <hansschmucker@gmail.com>
2026-03-05 10:21:25 +0100

    Update String.prototype.toBase64.js

--- String.prototype.toBase64.js
+++ String.prototype.toBase64.js
@@ -1,3 +1,12 @@
+/**
+ * Encodes the current string to base64
+ * @method
+ * @instance
+ * @memberOf String
+ * @name toBase64
+ * @returns {String}
+ */
+
 String.prototype.toBase64=function(){
   return window.btoa(this);
 };
