a73be9274fb0e29483819bc00d5aac5a06a05f92
hansschmucker <hansschmucker@gmail.com>
2026-03-05 10:17:39 +0100

    Update Object.prototype.toJson.js

--- Object.prototype.toJson.js
+++ Object.prototype.toJson.js
@@ -1,3 +1,13 @@
-Object.defineProperty(Object.prototype,"toJson",{value:function toJson(sep,flags){
-  return JSON.stringify(this,sep,flags);
+/**
+ * Json stringifies current object
+ * @method
+ * @instance
+ * @memberOf Object
+ * @name toJson
+ * @param replacer {function} Replacer
+ * @param Spacer {string} Space
+ * @returns {string}
+ */
+Object.defineProperty(Object.prototype,"toJson",{value:function toJson(repl,spc){
+  return JSON.stringify(this,repl,spc);
 }});
