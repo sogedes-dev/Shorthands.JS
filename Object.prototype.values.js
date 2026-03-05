c0bdf307216e3ba3bcdb34036ef44604a4397521
hansschmucker <hansschmucker@gmail.com>
2026-03-05 10:18:01 +0100

    Update Object.prototype.values.js

--- Object.prototype.values.js
+++ Object.prototype.values.js
@@ -1,3 +1,12 @@
+/**
+ * Returns all own values of object
+ * @method
+ * @instance
+ * @memberOf Object
+ * @name values
+ * @returns {Array}
+ */
+
 Object.defineProperty(Object.prototype,"values",{value:function values(){
   return Object.values(this);
 }});
