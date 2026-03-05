ecb5d052f94f0efd54e7a7f715901ae7a6051729
hansschmucker <hansschmucker@gmail.com>
2026-03-05 10:19:40 +0100

    Update String.prototype.asBase64.js

--- String.prototype.asBase64.js
+++ String.prototype.asBase64.js
@@ -1,3 +1,12 @@
+/**
+ * Decodes the current string as base64
+ * @method
+ * @instance
+ * @memberOf String
+ * @name keys
+ * @returns {string}
+ */
+
 String.prototype.asBase64=function(){
   return window.atob(this);
 };
