d4d1fd010d8df9e74cf085d7cae4e9f5a9ed9c4b
hansschmucker <hansschmucker@gmail.com>
2026-03-05 10:24:15 +0100

    Update sleep.js

--- sleep.js
+++ sleep.js
@@ -1,3 +1,10 @@
+/**
+ * Sleeps a specified number of Milliseconds until fulfillment
+ * @name sleep
+ * @params time {number} Milliseconds until the promise is fullfilled
+ * @returns {Promise}
+ */
+
 function sleep(ms){
   return new Promise((ok,err)=>{
     window.setTimeout(ok,ms);
