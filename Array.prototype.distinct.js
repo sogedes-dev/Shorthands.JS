a966847481c247898846d2e71b7120ffb97a8643
hansschmucker <hansschmucker@gmail.com>
2026-03-05 09:15:48 +0100

    Update Array.prototype.distinct.js

--- Array.prototype.distinct.js
+++ Array.prototype.distinct.js
@@ -1,7 +1,13 @@
+/** @class Array */
 /**
  * Returns all distinct values of an array
+ * @method
+ * @instance
+ * @memberOf Array
+ * @name distinct
+ * @returns {Array}
+ * @param serialize {boolean} If set will return distinct JSON-serialized (deserialized again after distinct)
  */
-
 Object.defineProperty(Array.prototype,"distinct",{value:function distinct(serialize) {
     if(serialize){
         return this.map(a=>a.toJson()).distinct().map(a=>a.asJson());
