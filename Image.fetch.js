59193d4074c1f95996c50c2cad22b1b2ae47e51d
hansschmucker <hansschmucker@gmail.com>
2026-03-05 10:09:46 +0100

    Update Image.fetch.js

--- Image.fetch.js
+++ Image.fetch.js
@@ -1,3 +1,11 @@
+/**
+ * Fetches a url as image asynchronously
+ * @memberOf Image
+ * @name fetch
+ * @param path {string} Url of the image to be fetched
+ * @returns {Promise<Image>}
+ */
+
 Image.fetch=function(path){
     var img=document.createElement("img");
     return new Promise(function(resolve,reject){
