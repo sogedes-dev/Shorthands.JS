611da6dcf9b606082d2bf99762fc8c0af73628ca
hansschmucker <hansschmucker@gmail.com>
2026-03-05 10:12:44 +0100

    Update Object.prototype.freeze.js

--- Object.prototype.freeze.js
+++ Object.prototype.freeze.js
@@ -1,3 +1,12 @@
+/**
+ * Calls freeze on the current object to turn it write protcted and returns the current object
+ * @method
+ * @instance
+ * @memberOf Object
+ * @name freeze
+ * @returns {Object}
+ */
+
 Object.defineProperty(Object.prototype,"freeze",{value:function freeze(){
   return Object.freeze(this);
 }});
