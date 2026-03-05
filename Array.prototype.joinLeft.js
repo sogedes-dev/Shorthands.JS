852e0fdb5af6cb8d56337d96ea4af9fc3016d6b0
hansschmucker <hansschmucker@gmail.com>
2026-03-05 09:26:21 +0100

    Update Array.prototype.joinLeft.js

--- Array.prototype.joinLeft.js
+++ Array.prototype.joinLeft.js
@@ -1,3 +1,14 @@
+/** @class Array */
+/**
+ * Groups an array into a nested tree meaning for each param the array that would be an array turns itself into a value:results dictionary
+ * @method
+ * @instance
+ * @memberOf Array
+ * @name joinLeft
+ * @returns {[string,Array]}
+ * @param secondArray {Array} Array to be joined
+ * @param matcher {function} A callback to be called with each line of self (with params(selfLine,secondArrayLine)) to determine a match
+ */
 Object.defineProperty(Array.prototype,"joinLeft",{value:function joinLeft(arr,match) {
     return this.map(a=>[a,arr.filter(b=>match(a,b))]);
 }});
