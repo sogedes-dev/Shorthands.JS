996a2e37d102c498b64f816f9266a161b2f6fe9f
hansschmucker <hansschmucker@gmail.com>
2026-03-05 09:31:59 +0100

    Update Array.prototype.sum.js

--- Array.prototype.sum.js
+++ Array.prototype.sum.js
@@ -1,3 +1,12 @@
+/**
+ * Returns the sum of all numbers in the array
+ * @method
+ * @instance
+ * @memberOf Array
+ * @name max
+ * @returns {number}
+ */
+
 Object.defineProperty(Array.prototype,"sum",{value:function sum() {
     return this.reduce((p,n)=>p+n,0);
 }});
