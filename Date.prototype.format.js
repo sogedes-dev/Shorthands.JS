3dc8f56bcb91c05919787be7f8c65c1813f80676
hansschmucker <hansschmucker@gmail.com>
2026-03-05 09:36:43 +0100

    Update Date.prototype.format.js

--- Date.prototype.format.js
+++ Date.prototype.format.js
@@ -1,3 +1,12 @@
+/**
+ * Returns a string representing the given date. Supported are yyyy(Year4), yy(Year2), MM(Month2), dd(Date2), d(Date), HH(Hour2), mm(Minutes2), ss(Seconds2)
+ * @method
+ * @instance
+ * @memberOf Date
+ * @name format
+ * @param formatString {string} formatString to use for laying out the date
+ * @returns {string}
+ */
 Date.prototype.format=function(str){
 	return str.
 		replace("yyyy",this.getFullYear().toString().padStart(4,'0')).
