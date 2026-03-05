98cdb6393f5b1d89edc72b2343feebda975b57a1
hansschmucker <hansschmucker@gmail.com>
2026-03-05 10:12:06 +0100

    Update Object.prototype.defineProperty.js

--- Object.prototype.defineProperty.js
+++ Object.prototype.defineProperty.js
@@ -1,3 +1,14 @@
+/**
+ * Calls defineProperty on the current object and returns the current object
+ * @method
+ * @instance
+ * @memberOf Object
+ * @name clone
+ * @param name {string} Name of the property
+ * @param propertyDescriptor {Object} Configuration for the property
+ * @returns {Object}
+ */
+
 Object.defineProperty(Object.prototype,"defineProperty",{value:function defineProperty(name,property){
   Object.defineProperty(this,name,typeof(property)=="object"?property:{value:property});
   return this;
