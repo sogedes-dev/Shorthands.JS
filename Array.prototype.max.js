d3b30a50ef5c6048d2d628ab167ef0f614e6aac2
hansschmucker <hansschmucker@gmail.com>
2026-03-05 09:31:24 +0100

    Update Array.prototype.max.js

--- Array.prototype.max.js
+++ Array.prototype.max.js
@@ -1,3 +1,12 @@
+/**
+ * Returns the highest value in the array
+ * @method
+ * @instance
+ * @memberOf Array
+ * @name max
+ * @returns {number}
+ */
+
 Object.defineProperty(Array.prototype,"max",{value:function max() {
     return Math.max(...this);
 }});
