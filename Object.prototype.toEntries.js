3281171b349d34ec203dbb7f4dba0f570613e8a1
hansschmucker <hansschmucker@gmail.com>
2026-03-05 10:15:36 +0100

    Update Object.prototype.toEntries.js

--- Object.prototype.toEntries.js
+++ Object.prototype.toEntries.js
@@ -1,3 +1,11 @@
+/**
+ * Returns all own properties as Array of 2 entry name,value arrays
+ * @method
+ * @instance
+ * @memberOf Object
+ * @name toEntries
+ * @returns {Array}
+ */
 Object.defineProperty(Object.prototype,"toEntries",{value:function toEntries(){
   return Object.entries(this);
 }});
