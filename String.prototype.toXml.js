fd5580223cd067b02763d661d6b00b09cefa12e0
hansschmucker <hansschmucker@gmail.com>
2026-03-05 10:23:06 +0100

    Update String.prototype.toXml.js

--- String.prototype.toXml.js
+++ String.prototype.toXml.js
@@ -1,3 +1,12 @@
+/**
+ * Parses the current string as XML and returns the documentElement
+ * @method
+ * @instance
+ * @memberOf String
+ * @name toXml
+ * @returns {Element}
+ */
+
 String.prototype.toXml=function(){
     return (new DOMParser().parseFromString(this, "application/xml")).documentElement;
 };
